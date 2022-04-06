d4d810e04c738cb67b2fa314a0b497d8426af5f3
AbhilashBalaji <abhilash.balaji1998@gmail.com>
2022-04-06 15:10:42 +0200

    implemented a few apis

--- assignment1/hello.py
+++ assignment1/hello.py
@@ -1,7 +1,72 @@
-from flask import Flask
+from crypt import methods
+import hashlib
+import random
+import re
+from flask import Flask, request, abort, jsonify, redirect , url_for
+
 
 app = Flask(__name__)
+storage = {"1":"2"}
+regex = re.compile(
+        r'^(?:http)s?://'
+        r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|'
+        r'localhost|'
+        r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})'
+        r'(?::\d+)?'
+        r'(?:/?|[/?]\S+)$', re.IGNORECASE)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def url_valid(url):
+	return re.match(regex,url) is not None
+
+
+
+@app.route("/", methods=['POST'])
+def shorten():
+#  create KV pair 
+	url = request.json['url']
+	if url_valid(url):
+		hash_url  = str(hashlib.shake_256(url.encode("UTF-8")).hexdigest(length=3))
+		storage[hash_url] = url
+		return str(hash_url), 201
+	else:
+		# url entered is not valid. TEST
+		return "URL Not valid format.", 400
+
+@app.route("/<int:potato_id>", methods=['GET'])
+def potato(potato_id):
+
+	'''
+	id handling.
+	'''
+	# return 'potato there'
+	return jsonify({"POTATO ID":potato_id})
+
+
+@app.route("/",methods=["GET"])
+def getAllPotatoes():
+	return jsonify({"IDs":list(storage.keys())})
+
+@app.route("/<int:delete_potato>", methods=['DELETE'])
+def potatodelete(delete_potato):
+	if delete_potato is None:
+		'''
+		/ endpoint.
+		'''
+	else:
+		'''
+		id handling.
+		'''
+
+
+
+
+
+
+
+def bad_request(message):
+
+	response = jsonify({'message': message})
+	response.status_code = 400
+	return response
+
+
