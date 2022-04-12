cf5357154ff0cdff2866a19120a11ef51792876f
KimAta <k.ataiyan@yahoo.com>
2022-04-12 16:13:39 +0200

    first commit

--- assignment 2/hello.py
+++ assignment 2/hello.py
@@ -20,26 +20,42 @@ regex = re.compile(
 def url_valid(url):
     return re.match(regex, url) is not None
 
+def user_valid(token):
+
+    #token passed in header i.e. userservice.istokenvalid(token)
+    return true;
+
+
 
 @app.route("/", methods=['POST'])
 def shorten():
-    #  create KV pair
-    url = request.json['url']
-    if url_valid(url):
-        hash_url = str(hashlib.shake_256(url.encode("UTF-8")).hexdigest(3))
-        storage[hash_url] = url
-        return str(hash_url), 201
+
+    if(user_valid):
+   	 #  create KV pair
+    	url = request.json['url']
+    	if url_valid(url):
+        	hash_url = str(hashlib.shake_256(url.encode("UTF-8")).hexdigest(3))
+        	storage[hash_url] = url
+       		return str(hash_url), 201
+    	else:
+        	# url entered is not valid. TEST
+       		 return "URL Not valid format.", 400
+
     else:
-        # url entered is not valid. TEST
-        return "URL Not valid format.", 400
+	return "forbidden", 301
 
 
 @app.route("/<potato_id>", methods=['GET'])
 def potato(potato_id):
-    if potato_id in storage.keys():
-        return redirect(storage[potato_id]), 301
+
+    if(url_valid):	
+   	 if potato_id in storage.keys():
+        	return redirect(storage[potato_id]), 301
+   	 else:
+        	return "short url not found.", 404
+
     else:
-        return "short url not found.", 404
+	return "forbidden", 403
 
 
 @app.route("/", methods=["GET"])
@@ -49,28 +65,37 @@ def getAllPotatoes():
 
 @app.route("/<id>", methods=['DELETE'])
 def potatodelete(id):
-    if id in storage.keys():
-        del storage[id]
-        return "successfully deleted", 204
+    if(url_valid):
+    	if id in storage.keys():
+        	del storage[id]
+        	return "successfully deleted", 204
+   	 else:
+        	return "Shortened URL not found", 404
     else:
-        return "Shortened URL not found", 404
+	return "forbidden",403
 
 
 @app.route("/", methods=['DELETE'])
 def potatodontdelete():
-    return "", 404
+    if(url_valid):
+   	 return "", 404
+    else:
+	return "forbidden",403
 
 
 @app.route("/<shorturl>", methods=["PUT"])
 def update(shorturl):
-    # change actual url for a given short url
-    # long url is in the request body.
-    longurl = request.json['longurl']
-    if url_valid(longurl) == False:
-        return "URL to be shortened is invalid. ", 400
-    for Sshort, Slong in storage.items():
-        if shorturl == Sshort and url_valid(longurl):
-            storage[Sshort] = longurl
-            return "successful updation.", 200
-
-    return "shortend url not found", 404
+    
+    if(url_valid):
+   	 # change actual url for a given short url
+   	 # long url is in the request body.
+    	longurl = request.json['longurl']
+    	if url_valid(longurl) == False:
+        	return "URL to be shortened is invalid. ", 400
+    	for Sshort, Slong in storage.items():
+        	if shorturl == Sshort and url_valid(longurl):
+           		 storage[Sshort] = longurl
+        	    	 return "successful updation.", 200
+	return "shortend url not found", 404
+    else:
+	return "forbidden", 403
