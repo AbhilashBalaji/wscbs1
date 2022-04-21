7031c212d093ef3b464d3384502c16c86fc9563d
AbhilashBalaji <abhilash.balaji1998@gmail.com>
2022-04-21 17:32:57 +0200

    added a script.

--- assignment 2/run.sh
+++ assignment 2/run.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
-
+# Kinda buggy use at your own risk.
+trap ctrl_c INT
+function ctrl_c() {
+    PORT_NUMBER=6000
+    lsof -i tcp:${PORT_NUMBER} | awk 'NR!=1 {print $2}' | xargs kill 
+    PORT_NUMBER=6001
+    lsof -i tcp:${PORT_NUMBER} | awk 'NR!=1 {print $2}' | xargs kill
+    PORT_NUMBER=5000
+    lsof -i tcp:${PORT_NUMBER} | awk 'NR!=1 {print $2}' | xargs kill  
+}
 export FLASK_APP=api_gateway
 flask run --port 5000 &
 
