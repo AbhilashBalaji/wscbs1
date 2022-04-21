f6802a7b65d1fa1df37afef19834afb9535f9f33
AbhilashBalaji <abhilash.balaji1998@gmail.com>
2022-04-21 15:12:51 +0200

    changed names

--- assignment 2/run.sh
+++ assignment 2/run.sh
@@ -3,8 +3,8 @@
 export FLASK_APP=api_gateway
 flask run --port 5000 &
 
-export FLASK_APP=hello
+export FLASK_APP=shortner
 flask run --port 6000 &
 
-export FLASK_APP=behnam
+export FLASK_APP=users
 flask run --port 6001 &
